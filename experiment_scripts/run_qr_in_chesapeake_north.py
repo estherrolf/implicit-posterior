4ca76cbd7c3cbc1b82cedc9807cde82f1ebd720d
estherrolf <esther_rolf@berkeley.edu>
2022-02-23 17:28:05 +0000

    setting gpu to 0

--- experiment_scripts/run_qr_in_chesapeake_north.py
+++ experiment_scripts/run_qr_in_chesapeake_north.py
@@ -49,7 +49,7 @@ def main():
     ):
         experiment_name = f"{states_str}_{model}_{lr}_{loss}_{prior_version}_additive_smooth_{additive_smooth}_prior_smooth_{prior_smooth}"
 
-        output_dir = "../output_rep/chesepeake_north_qr"
+        output_dir = "../output_rep/chesapeake_north_qr_rep"
           
         train_splits = [f'{state}-{train_set}' for state in states_str.split('+')]
         val_splits = [f'{state}-{val_set}' for state in states_str.split('+')]
@@ -74,7 +74,7 @@ def main():
             f"experiment.datamodule.val_splits={val_splits}",
             f"experiment.datamodule.test_splits={test_splits}",
             f"program.output_dir={output_dir}",
-            f"program.log_dir=../logs/chesapeake_north_qr_rep",
+            f"program.log_dir=../logs/chesapeake_north_qr_rep1",
             "trainer.max_epochs=200",
             "trainer.gpus=[GPU]"
         ]
